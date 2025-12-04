f577f2cbe36bd085cfa87b03f86841e1c1f73e59
Purushoth <vpurushoth97@gmail.com>
2025-12-04 10:46:23 -0800

    adding auth T2

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,24 @@
+// import { defineAuth } from '@aws-amplify/backend';
+
+// /**
+//  * Define and configure your auth resource
+//  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+//  */
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: true,
+//   },
+// });
+
+import { defineAuth } from "@aws-amplify/backend";
+
+export const auth = defineAuth({
+  loginWith: {
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to the AI-Powered Recipe Generator!",
+      verificationEmailBody: (createCode) =>
+        `Use this code to confirm your account: ${createCode()}`,
+    },
+  },
+});
